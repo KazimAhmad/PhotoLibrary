dc679c5d0337d41a1d1023866f5754cae5cb3306
Kazim Ahmad <bitchy_kazim@Kazims-MacBook-Pro.local>
2026-01-19 04:34:40 +0000

    new project

---
+++ PhotoLibrary/PhotoLibrary/PhotoLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoLibraryApp.swift
+//  PhotoLibrary
+//
+//  Created by Kazim Ahmad on 19/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct PhotoLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
